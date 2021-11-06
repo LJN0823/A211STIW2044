e6e635ef9c13fbf45e2e7e58763694a71af276cd
LJN0823 <92237242+LJN0823@users.noreply.github.com>
2021-11-06 16:18:42 +0800

    Update currencyconvert.dart

--- currencyconvert.dart
+++ currencyconvert.dart
@@ -1,7 +1,5 @@
 class CurrencyConvert{
-  //var myr = 0.0, jpy = 0.0, krw = 0.0, cny = 0.0, sgd = 0.0 , loc = "not valiable";
   var value = 0.0;
 
-  //CurrencyConvert(this.myr,this.jpy,this.krw,this.cny, this.sgd, this.loc);
   CurrencyConvert(this.value);
 }
